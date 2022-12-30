335f0d6668f281687856ab183592a6030638a40c
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-12-30 16:53:04 +0100

    change begin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Configuration
 discordBotToken = "MTA1Njg4MDEzMTgzNjIzMTc1MQ.GrH7J6.tEfOxOCllo2cB89LJKF3A1Yq9vH6Z--1o2-7tM"
 moderatorID = [461807010086780930, 374173210519928832,280804426376151041]
-commandPrefix = "<"
+commandPrefix = "?"
 channelQuestionID = 1058101751120674967
 timeleft = 30 # in minutes
 # =================================================================================================================================================================
